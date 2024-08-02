62471f51ed4120a8edd64ab6696e9f5a94b6e4d9
SanjayanKarthikeyan <146628512+SanjayanKarthikeyan@users.noreply.github.com>
2024-08-02 10:22:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const dbconnect = require("./shared/db_integration");
+const userRouter = require("./routes/userRoute");
+
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("called " + req.url + " by " + req.ip);
+  next();
+});
+
+app.use("/user", userRouter);
+
+dbconnect()
+  .then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+      console.log("Server Running Successfully");
+    });
+  })
+  .catch((e) => console.log(e));
